8f31eb86d2251318788d4f86ecc21375f392f616
Shahin Safarli <88702436+shahinsafarli@users.noreply.github.com>
2026-02-17 10:53:34 +0400

    Add files via upload

--- StringUtil.java
+++ StringUtil.java
@@ -34,12 +34,30 @@ public class StringUtil {
         return new String(arr);
     }
 
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        else {
+
+            char [] arr1 = s1.toCharArray();
+            char [] arr2 = s2.toCharArray();
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            return Arrays.equals(arr1, arr2);
+        }
+    }
+
     public static void main(String[] args) {
 
         String s1 = "Hello";
         System.out.println(reverse(s1));
         System.out.println(explode(s1));
         System.out.println(sortString(s1));
+        System.out.println(isAnagram("map", "pam"));
 
 
     }
